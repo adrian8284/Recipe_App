87ff5ec4acdd0782ba86740fd6a784c41d37ef1c
Adrian <adrianagustin564@gmail.com>
2025-03-30 22:04:38 -0700

    Changes to forms

--- app131-db-2/app/forms.py
+++ app131-db-2/app/forms.py
@@ -1,5 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SubmitField, validators
+from wtforms.validators import DataRequired, Length
+
+# Added RecipeForm to collect recipe details
+class RecipeForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(max=80)])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    ingredients = TextAreaField('Ingredients', validators=[DataRequired()])
+    instructions = TextAreaField('Instructions', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 class LoginForm(FlaskForm):
     username = StringField('USERNAME', validators=[validators.DataRequired()])
