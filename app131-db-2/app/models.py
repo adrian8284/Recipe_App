828cad00dff60b9d9faa6f7cd7093eecff1a6210
Adrian <adrianagustin564@gmail.com>
2025-03-30 21:41:08 -0700

    Small change to models, time now timezone aware.

--- app131-db-2/app/models.py
+++ app131-db-2/app/models.py
@@ -20,5 +20,5 @@ class Recipe(db.Model):
     description = db.Column(db.Text, nullable=False)
     ingredients = db.Column(db.Text, nullable=False)
     instructions = db.Column(db.Text, nullable=False)
-    created = db.Column(db.DateTime, default=datetime.utcnow)
+    created = db.Column(db.DateTime, default=datetime.timezone.utc)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
