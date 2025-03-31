2dbc9f75d34fe35e917afb0e4570b5f4d98c781f
Adrian <adrianagustin564@gmail.com>
2025-03-30 21:37:37 -0700

    Changes to models

--- app131-db-2/app/models.py
+++ app131-db-2/app/models.py
@@ -1,7 +1,24 @@
 from app import db
+from flask_login import UserMixin
+from datetime import datetime
+from app import login_manager
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32))
-    password = db.Column(db.String(32))
-    email = db.Column(db.String(32))
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    recipes = db.relationship('Recipe', backref='author', lazy=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
